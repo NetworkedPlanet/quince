bd554719780199b390470225c01ac9aa55eafeb3
Kal <kal@techquila.com>
2018-12-21 12:14:41 +0000

    Add simple test import program

---
+++ NetworkedPlanet.Quince.Import/Options.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetworkedPlanet.Quince.Import
+{
+    internal class Options
+    {
+        public string RepoDirectory { get; set; }
+        public string ImportFile { get; set; }
+        public Uri GraphUri { get; set; }
+    }
+}
